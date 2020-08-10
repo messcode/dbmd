54c3f05bf0bbb3d4bc15a709ebc4584ca9ff5d23
Dabiao Zhang <zhang.dabiao11@gmail.com>
2020-08-09 17:00:06 -0700

    add install export_fig shell script

---
+++ src/main/install-export_fig.sh
@@ -0,0 +1,5 @@
+# intall export_fig for matlab visulization.
+curl -L0 https://github.com/altmany/export_fig/archive/v3.11.zip > matlab/com/altmany-export_fig.zip
+unzip matlab/com/altmany-export_fig -d matlab/com/
+mv matlab/com/export_fig-3.11/  matlab/com/altmany-export_fig
+rm matlab/com/altmany-export_fig.zip
